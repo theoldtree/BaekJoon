27195675fce848bb161e02fed6dcb10301820c8a
theoldtree <yjeongmu@gmail.com>
2021-01-25 22:10:32 +0900

    코드업 1099코멘트 추가

--- Python_codeup/solution1099.py
+++ Python_codeup/solution1099.py
@@ -1,7 +1,9 @@
 matrix = []
 for index in range(10):
     struct_list = list(map(int,input().split()))
-    # TODO 배열 = 리스트 -> 배열에 추가할때 list의 형태로 전환해줘야 한다.
+    # TODO 배열 = 리스트 -> 배열에 추가할때 list의 형태로 전환해줘야 함
+    # 숫자 관련해서 다룰띠는 int로 전환해주는거 있지 않기
+    # map을 사용하여 리스트안의 내용물을 하나하나 다룰수 있음
     matrix.append(struct_list) 
 row, col = 1,1
 while True:
